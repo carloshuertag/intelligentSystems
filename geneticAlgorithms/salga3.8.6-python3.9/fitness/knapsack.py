29f29c777da1cddefe98e84e290f7a14ac96d66a
Carlos Huerta Garcia <huerta2502@gmail.com>
2023-03-16 12:46:29 +0100

    nqueens n knapsack problem added'

---
+++ geneticAlgorithms/salga3.8.6-python3.9/fitness/knapsack.py
@@ -0,0 +1,29 @@
+import random
+
+n = 10
+
+def generate_random_knapsack_problem(num_items):
+    items = []
+    for i in range(num_items):
+        weight = random.randint(1, 10)
+        value = random.randint(1, 20)
+        items.append((f'item{i}', weight, value))
+    knapsack_capacity = random.randint(num_items*2, num_items*10)
+    return items, knapsack_capacity
+
+alphabet, capacity = generate_random_knapsack_problem(n)
+
+def phenotype(chromosome):
+    return f'{len(chromosome)}\n'.join([f'{item[0]}: {item[1]}kg, {item[2]}€' for item in chromosome])
+
+def fitness(chromosome):
+    total_weight = 0
+    total_value = 0
+    for item in chromosome:
+            total_weight += item[1]
+            total_value += item[2]
+    if total_weight > capacity:
+        return 1.0 / (1.0 + abs(total_weight - capacity) / capacity)
+    return 1.0 / (1.0 + (total_value / (20 * n)))
+
+parameters = {'alphabet': alphabet, 'type': 'classic', 'elitism': False, 'norm': True, 'chromsize': random.randint(1,n), 'pmut': 0.2}
---
+++ geneticAlgorithms/salga3.8.6-python3.9/fitness/nqueens.py
@@ -0,0 +1,29 @@
+n = 12
+alphabet = [(x,y) for x in range(n) for y in range(n)]
+
+def phenotype (chromosome):
+    emptyRow = ['□'] * n
+    board = ''
+    currentRow = ''
+    for i in range(n):
+        currentRow = emptyRow[:]
+        for j in range(n):
+            if (i,j) in chromosome:
+                currentRow[j] = '■'
+        board += ' '.join(currentRow)
+        board += '\n'
+    return board
+
+def fitness (chromosome):
+    if len(chromosome) != len(set(chromosome)):
+        return 0.0 # repeated queens are not allowed
+    error = 0.0
+    for i in range(len(chromosome)):
+        for j in range(i+1, len(chromosome)):
+            if chromosome[i][0] == chromosome[j][0] or chromosome[i][1] == chromosome[j][1]:
+                error += 1.0 / n # queens in the same row or column are not allowed
+            if abs(chromosome[i][0] - chromosome[j][0]) == abs(chromosome[i][1] - chromosome[j][1]):
+                error += 1.0 / n # queens in the same diagonal are not allowed
+    return 1.0 / (1.0 + error)
+
+parameters = { 'alphabet':alphabet, 'type':'classic', 'elitism':False, 'norm':True, 'chromsize':n, 'pmut':0.2 }
