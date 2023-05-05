c2d6313265090290eecbdbd0ff53785c3b02725f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2023-05-05 08:50:30 +0000

    add:  dqn agent & notebook
    falta comentar

--- dql/lunar_lander_dql.py
+++ dql/lunar_lander_dql.py
@@ -1,98 +1,120 @@
-from typing import Callable, List, NamedTuple, Sequence, SupportsFloat, Union
-from collections import deque
 import random
 import gymnasium as gym
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 import numpy as np
+from collections import deque
 
-class Transition(NamedTuple):
-    prev_state: Any
-    next_state: Any
-    action: Any
-    reward: SupportsFloat
-    terminated: bool
+class DQLAgent:
+    def __init__(self, env):
+        self.state_size = env.observation_space.shape[0]
+        self.action_size = env.action_space.n
+        self.epsilon = 1
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.9993
+        self.gamma = 0.99
+        self.learning_rate = 0.0001
+        self.memory = deque(maxlen=4000)
+        self.model = self.build_model()
+        self.target_model = self.build_model()
 
-class Memory:
-    def __init__(self, size: int):
-        self.max_size = int(size)
-        self.transitions: deque = deque(maxlen=self.max_size)
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(64, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
+        return model
 
-    def remember(self, transition: Transition):
-        self.transitions.append(transition)
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
-    def batch(self, n: int) -> List[Transition]:
-        n = min(len(self.transitions), n)
-        return random.sample(self.transitions, n)
+    def act(self, s):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        act_values = self.model.predict(s)
+        return np.argmax(act_values[0])
 
-    def __len__(self) -> int:
-        return len(self.transitions)
+    def replay(self,batch_size):
+        "vectorized replay method"
+        if len(agent.memory) < batch_size:
+            return
 
-    def __getitem__(self, key: Union[int, slice]
-    ) -> Union[Transition, Sequence[Transition]]:
-        return self.transitions.__getitem__(key)
+        minibatch = random.sample(self.memory, batch_size)
+        minibatch = np.array(minibatch)
+        not_done_indices = np.where(minibatch[:, 4] == False)
+        y = np.copy(minibatch[:, 2])
 
-class Agent:
-    def __init__(self, *,
-        env: gym.Env,
-        model: Union[Callable[[int, int], tf.keras.Model], tf.keras.Model, str],
-        batch_size: int = 32,
-        memory_size: int = 1e5,
-        gamma: float = 0.99
-    ):
-        self.env = env
-        self.inputs = env.observation_space.shape[0]
-        self.actions = env.action_space.n
-        self.batch_size = batch_size
-        self.gamma = gamma
-        self.memory = Memory(memory_size)
-        self.current_state = None
-        self.assign_model(model)
+        if len(not_done_indices[0]) > 0:
+            predict_sprime = self.model.predict(np.vstack(minibatch[:, 3]))
+            predict_sprime_target = self.target_model.predict(np.vstack(minibatch[:, 3]))
+            
+            y[not_done_indices] += np.multiply(self.gamma, predict_sprime_target[not_done_indices, np.argmax(predict_sprime[not_done_indices, :][0], axis=1)][0])
+
+        actions = np.array(minibatch[:, 1], dtype=int)
+        y_target = self.model.predict(np.vstack(minibatch[:, 0]))
+        y_target[range(batch_size), actions] = y
+        self.model.fit(np.vstack(minibatch[:, 0]), y_target, epochs=1, verbose=0)
         
-    def assign_model(self, model: Union[Callable[[int, int], tf.keras.Model], tf.keras.Model, str]) :
-        if(callable(model)):
-            self.model = model(self.inputs, self.actions)
-        elif(isinstance(model, tf.keras.Model)):
-            self.model = model
-        elif(isinstance(model, str)):
-            self.model = tf.keras.models.load_model(model)
-        else:
-            raise ValueError("model must be a function, tf.keras.Model, or a path to a model")
 
-    def task(self, max_iterations: int = None) -> float:
-        self.current_state, _ = self.env.reset()
-        max_iterations = max_iterations or np.inf
-        reward = 0
-        running = True
-        while running and max_iterations > 0:
-            self.current_step += 1
-            perception = self.perceive()
-            action = self.decide(perception)
-            transition = self.act(action)
-            reward += transition.reward
-            running = not transition.terminated
-        return reward
+            
+    def adaptiveEGreedy(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
     
-    def perceive(self):
-        return self.current_state
+    def targetModelUpdate(self):
+        self.target_model.set_weights(self.model.get_weights())
+            
+if __name__ == "__main__":
+    env = gym.make('LunarLander-v2')
+
+    state, _ = env.reset()
+
+    agent = DQLAgent(env)
+    state_number = env.observation_space.shape[0]
     
-    def decide(self, perception):
-        if random.random() < self.epsilon:
-            return self.env.action_space.sample()
-        else:
-            perception = perception[np.newaxis, ...]
-            q_values = self.model.predict(perception, verbose = 0)
-            return np.argmax(q_values[0])
+    batch_size = 32
+    episodes = 5
+    for e in range(episodes):
         
-    def act(self, action):
-        next_state, reward, terminated, _, _ = self.env.step(action)
-        self.memory.remember(Transition(
-            prev_state = self.current_state,
-            next_state = next_state, 
-            action = action,
-            reward = reward,
-            terminated = terminated))
-        self.current_state = next_state
-        return self.memory[-1]
-    
-class IOModel:
-    def __init__(self, inputs, outputs) -> tf.keras.Model:
+        state,_ = env.reset()
+        state = np.reshape(state, [1, state_number])
+
+        total_reward = 0
+        for time in range(1000):
+            
+
+            action = agent.act(state)
+            
+            next_state, reward, done, _,_ = env.step(action)
+            next_state = np.reshape(next_state, [1, state_number])
+
+            agent.remember(state, action, reward, next_state, done)
+
+            state = next_state
+
+            agent.replay(batch_size)
+
+            total_reward += reward
+            
+            if done:
+                agent.targetModelUpdate()
+                break
+
+        agent.adaptiveEGreedy()
+
+        print('Episode: {}, Reward: {}'.format(e,total_reward))
+        
+
+env = gym.make('LunarLander-v2', render_mode='human')
+
+state, _ = env.reset()
+
+terminated = truncated = False
+while not (terminated or truncated):
+    # Seleccionamos una acción dado el actual estado ...
+    action = agent.act(state)
+    # ... y la ejecutamos, obteniendo el nuevo estado
+    state, _, terminated, truncated, _ = env.step(action)
+env.close()
